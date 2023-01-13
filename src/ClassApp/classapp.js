14d6fd767cde2257b77de6182545800c6fa7b5be
tadtechx <tadtechcreative@gmail.com>
2023-01-13 15:24:31 +0100

    new

--- src/ClassApp/classapp.js
+++ src/ClassApp/classapp.js
@@ -6,8 +6,8 @@ import axios from "axios";
 
 import style from "./classapp.css";
 
-import {FaUser, FaUsers, FaComment} from "react-icons/fa";
-import {BiRefresh} from "react-icons/bi";
+import { FaUser, FaUsers, FaComment, FaTrash } from "react-icons/fa";
+import { BiRefresh } from "react-icons/bi";
 
 
 import ClassMembers from "./classmembers";
@@ -16,8 +16,9 @@ import ClassQuestions from "./classquestions";
 import { useCookies } from 'react-cookie';
 
 
+
 function ClassApp(props) {
-    
+
     const [cookies, setCookie, removeCookie] = useCookies(['user']);
 
 
@@ -25,22 +26,12 @@ function ClassApp(props) {
     const [questions, setQuestions] = useState(false);
     const thisIframe = useRef();
 
-    
 
 
-//   const instructorMessages = [
-//     "HTML is a markup language used to structure a webpage/site",
-//     "CSS stands for Cascading Style Sheet, it is used for styling",
-//     "Javascript is a programming language, it is used in web development to write functions",
-//     "React is a javascript framework designed by Facebook",
-//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, autem maxime aliquid blanditiis optio asperiores quis voluptas fugiat quo, dolore esse ad quos nam magnam provident distinctio. Dolores et perspiciatis, eligendi quod animi corporis quis rerum adipisci illum laboriosam sed vitae quo iste minus officia quidem unde, non distinctio."
-
-//   ]
-    
     const reloadIFrame = () => {
-            console.log('reloading..');
-            thisIframe.current.src = thisIframe.current.src;
-        }
+        console.log('reloading..');
+        thisIframe.current.src = thisIframe.current.src;
+    }
 
 
 
@@ -51,7 +42,7 @@ function ClassApp(props) {
         setQuestions(value => !value);
     }
 
-    
+
     const [iMessages, setIMessages] = useState("");
     const theMsg = {
         Msg: iMessages,
@@ -63,23 +54,23 @@ function ClassApp(props) {
 
 
         axios
-        .post("/api/messages/mainmessage", theMsg)
-        .then(res => {
-          console.log(res);
-          instructorMessages.push(res.data.message);
-          console.log(instructorMessages);
-        })
-        .catch(err => {
-          const errors = err.response.data;
-            console.log(err.response.data)
-      });
-        
-      };
-      
-      const onChange = (a) => {
+            .post("/api/messages/mainmessage", theMsg)
+            .then(res => {
+                // console.log(res);
+                instructorMessages.push(res.data.message);
+                // console.log(instructorMessages);
+            })
+            .catch(err => {
+                const errors = err.response.data;
+                console.log(err.response.data)
+            });
+
+        document.getElementById("msg-box").value = "";
+    };
+
+    const onChange = (a) => {
         setIMessages(value => a);
-    
-        console.log(iMessages)
+
     }
 
 
@@ -87,25 +78,41 @@ function ClassApp(props) {
 
     const getMessages = () => {
         axios.get("api/messages/fetchmessages")
-          .then((response) => {
-            // console.log(response.data.filter(forThisClass=>{return forThisClass._id === "63c0209a2ffd9164036e5bb7"}));
-          setInstructorMessages(response.data.filter(forThisClass=>{return forThisClass.class === props.thisClass.name}));
-          });
-    
-      };
-  
-    //   getMessages()
+            .then((response) => {
+                // console.log(response.data.filter(forThisClass=>{return forThisClass._id === "63c0209a2ffd9164036e5bb7"}));
+                setInstructorMessages(response.data.filter(forThisClass => { return forThisClass.class === props.thisClass.name }).reverse());
+            });
+
+    };
+
+    getMessages();
+
+    const deleteMessage = (a) => {
+
+
+        console.log(instructorMessages[a]._id);
+        axios
+            .post("/api/messages/deletemessage", { id: instructorMessages[a]._id })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                const errors = err.response.data;
+                console.log(err.response.data)
+            });
+
+    }
 
     return (
         <div className="ClassApp">
-            <button onClick={getMessages}>get</button>
+            {/* <button onClick={deleteMessage}>get</button> */}
 
             <nav className="classapp-nav">
-               <Link to="/dashboard"><button><FaUser/></button></Link>
+                <Link to="/dashboard"><button><FaUser /></button></Link>
                 <h3>{props.thisClass.name}</h3>
                 <div>
-                    <button onClick={openMembers}><FaUsers/></button>
-                    <button onClick={openQuestions}><FaComment/></button>
+                    <button onClick={openMembers}><FaUsers /></button>
+                    <button onClick={openQuestions}><FaComment /></button>
                 </div>
             </nav>
             <div className="classapp-body">
@@ -116,47 +123,54 @@ function ClassApp(props) {
                 </div>
                 <div className="main-classbox">
                     <iframe className="code-edit"
-                    // style={{display: props.codePen}}
-                    //  src="https://codepen.io/eustace_dike/embed/ZEROdmq"
-                    //  src="https://codepen.io/harunpehlivan/full/qBNpGKr"
-                    // src="https://replit.com/team/team-tadpython/TadTech"
-                    src={props.pLanguage}
-                    ref={thisIframe}
-                     >
-                        
+                        // style={{display: props.codePen}}
+                        //  src="https://codepen.io/eustace_dike/embed/ZEROdmq"
+                        //  src="https://codepen.io/harunpehlivan/full/qBNpGKr"
+                        // src="https://replit.com/team/team-tadpython/TadTech"
+                        src={props.pLanguage}
+                        ref={thisIframe}
+                    >
+
                     </iframe>
-                    <button className="refresh" onClick={reloadIFrame}><BiRefresh/></button>
+                    <button className="refresh" onClick={reloadIFrame}><BiRefresh /></button>
 
                     <br />
-                    
+
 
                     <div className="class-messages">
                         <div className="class-comment">
                             <h4>Dean Mark (Instructor)</h4> <br />
 
                             {
-                                instructorMessages.map(imessage =>{
+                                instructorMessages.map(imessage => {
                                     return (
                                         <>
-                                        <p>{imessage.message} </p><br />
+                                            <p key={instructorMessages.indexOf(imessage)}>{imessage.message}
+                                                <span className="delete-msg"
+                                                    onClick={() => { deleteMessage(instructorMessages.indexOf(imessage)) }}
+                                                    style={{ display: cookies.Role === "instructor" ? "" : "none" }}
+                                                ><FaTrash /></span>
+                                            </p> <br />
+                                            {/* <p>{instructorMessages.indexOf(imessage)}</p> */}
                                         </>
                                     )
                                 })
                             }
-                            
+
                         </div>
 
-                        
-                        
-                       
+
+
+
                         <br />
                         <form
-                        style={{display: cookies.Role === "instructor"? "" : ""}}
-                        onSubmit={sendMessage} noValidate
+                            style={{ display: cookies.Role === "instructor" ? "" : "none" }}
+                            onSubmit={sendMessage} noValidate
                         >
                             <input type='text'
-                            onChange={(e)=>{onChange(e.target.value)}}
-                           />
+                                id="msg-box"
+                                onChange={(e) => { onChange(e.target.value) }}
+                            />
                             <button>send</button>
                         </form>
                     </div>
@@ -166,7 +180,7 @@ function ClassApp(props) {
                     style={{ transform: questions ? 'translateX(0%)' : null }}
 
                 >
-                    <ClassQuestions />
+                    <ClassQuestions Dept={props.thisClass.name}/>
                 </div>
 
 
--- src/ClassApp/classquestions.js
+++ src/ClassApp/classquestions.js
@@ -1,38 +1,133 @@
 import React from "react";
 
+import axios from "axios";
+
+import { useState } from "react";
+
+import { Cookies, useCookies } from 'react-cookie';
+
+
+
 import style from "./classapp.css"
 
-function ClassQuestions () {
+function ClassQuestions(props) {
+
+    const [cookies, setCookie, removeCookie] = useCookies(['user']);
 
 
+    const [question, setQuestion] = useState("");
 
+    const theQuestion = {
+        question: question,
+        Dept: props.Dept,
+        name: cookies.FirstName + " " + cookies.LastName
+    }
+    const sendQuestion = (e) => {
+
+        e.preventDefault();
+
+
+        axios
+            .post("/api/messages/sendquestion", theQuestion)
+            .then(res => {
+                console.log(res);
+                questionOutput.push(res.data.question);
+                // console.log(questionOutput);
+            })
+            .catch(err => {
+                const errors = err.response.data;
+                console.log(err.response.data)
+            });
+
+        document.getElementById("qstn-box").value = "";
+    };
+
+    const onChange = (a) => {
+        setQuestion(value => a);
+
+    }
+
+    const [questionOutput, setQuestionOutput] = useState([""]);
+
+    const getQuestions = () => {
+        axios.get("api/messages/fetchquestions")
+            .then((response) => {
+                setQuestionOutput(response.data.filter(forThisClass => { return forThisClass.class === props.Dept }).reverse());
+                // console.log(response.data.filter(forThisClass => { return forThisClass.class === props.Dept }).reverse());
+            });
+
+    };
+
+    getQuestions();
+
+
+    const [answer, setAnswer] = useState("");
+
+
+    const sendAnswer = (a) => {
+
+        // e.preventDefault();
+
+
+        axios
+            .post("/api/messages/sendanswer", { id: questionOutput[a]._id, answer: answer})
+            .then(res => {
+                console.log(res);
+                // questionOutput.push(res.data.question);
+                // console.log(questionOutput);
+            })
+            .catch(err => {
+                const errors = err.response.data;
+                console.log(err.response.data)
+            });
+
+        // document.getElementById("qstn-box").value = "";
+    };
 
     return (
         <div className="classdiv">
-            
-            <form>
-                <textarea rows="10" placeholder="Enter your question here">
-                </textarea> <br/>
+
+            <form onSubmit={sendQuestion} noValidate>
+                <textarea
+                    rows="10" placeholder="Enter your question here"
+                    id="qstn-box"
+                    onChange={(e) => { onChange(e.target.value) }}
+                >
+                </textarea> <br />
                 <button>Send</button>
             </form>
 
-            <br/> <br/>
+            <br /> <br />
+            <div className="all-class-questions">
 
-            <div className="class-comment">
-                <h4>Matt Damon</h4> <br/>
-                <p><b>Q: </b>What is the name of the IDE used for this project?</p>
-                <br/>
-                <h4>Instructor</h4> <br/>
-                <p><b>A: </b>VS Code</p>
-            </div>
-            <div className="class-comment">
-                <h4>Matt Damon</h4> <br/>
-                <p><b>Q: </b>What is the name of the IDE used for this project?</p>
-                <br/>
-                <h4>Instructor</h4> <br/>
-                <p><b>A: </b>VS Code</p>
-            </div>
-            
+            {
+                questionOutput.map(eachQuestion => {
+                    return (
+                            <div className="class-comment" key={questionOutput.indexOf(eachQuestion)}>
+                                <h4>{eachQuestion.name}</h4> <br />
+                                <p><b>Q: </b>{eachQuestion.question}</p>
+                                <br />
+                                <h4>Instructor</h4> <br />
+                                <input
+                                onChange={(e) => { setAnswer(e.target.value) }}
+                                style={{ display: cookies.Role === "instructor" ? "" : "none" }}
+                                type="text" />
+                                <button
+                                onClick={() => { sendAnswer(questionOutput.indexOf(eachQuestion)) }}
+                                style={{ display: cookies.Role === "instructor" ? "" : "none" }}
+
+                               >
+                                    reply
+                                </button>
+                                <p><b>A: </b>{eachQuestion.answer}</p>
+                            </div>
+                            
+                    )
+                })
+            }
+                        </div>
+
+
 
         </div>
     )
